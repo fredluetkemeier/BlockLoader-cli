eee3eec71da05ef9e7588663743f6185ae5a0da4
fredluetkemeier <fredluetkemeier@gmail.com>
2021-12-31 03:14:28 -0600

    Messing around with opts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use getopts::Options;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let mut opts = Options::new();
+    opts.optopt("", "install", "Install a mod", "NAME");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(val) => val,
+        Err(err) => {
+            panic!("{}", err)
+        }
+    };
 }
